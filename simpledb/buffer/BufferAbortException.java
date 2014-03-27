9a65dca13c16d6e024a92406e36e780c1b415aa4
philip-w-howard <phil.w.howard@gmail.com>
2014-03-27 11:21:06 -0700

    Netbeans Project ized the directory

--- simpledb/buffer/BufferAbortException.java
+++
@@ -1,9 +0,0 @@
-package simpledb.buffer;
-
-/**
- * A runtime exception indicating that the transaction
- * needs to abort because a buffer request could not be satisfied.
- * @author Edward Sciore
- */
-@SuppressWarnings("serial")
-public class BufferAbortException extends RuntimeException {}
